8fc53a49e6c8f6b010f054c52c27c1d929f0aa13
JuanRoa785 <roaporrasjuandiego@gmail.com>
2025-10-10 09:29:25 -0500

    Documentación clase #10

--- hello_world/tiposDatos.dart
+++ hello_world/tiposDatos.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 void tipos_datos(){
   //Tipos de datos en Dart
 
@@ -8,6 +10,7 @@ void tipos_datos(){
 
   //Tambien se puede nombrar variables con nomenclatura CamelCase:
   bool esVerdadero = true; // Booleano
+  //bool esFalso = 0; A diferencia de Python, Dart no traduce 0 a False y 1 a True.
 
   String texto = "Hola, Dart!"; // Cadena de texto (Es el unico tipo de dato que inicia con Mayuscula)
   String texto2 = 'Hola, Dart!'; // Cadena de texto con comillas simples
@@ -19,6 +22,9 @@ void tipos_datos(){
   de texto que abarca multiples lineas.
   Utilizando comillas triples simples.
   ''';
+
+  //Al definir una variable con 'var', Dart infiere el tipo de dato automáticamente:
+  var numero_inferido = 20; // Dart infiere que es un int
 
   var tipo_dato = numero.runtimeType; // Obtener el tipo de dato de una variable
   print("El tipo de dato de la variable 'numero' es: $tipo_dato");  
